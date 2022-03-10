a987b55355fa1b9d0851fa7d1053435447ef8f90
Rugved Wagh <89897353+Rugved76@users.noreply.github.com>
2022-03-10 15:30:01 +0530

    Update Circular_Queue.cpp

--- Circular_Queue.cpp
+++ Circular_Queue.cpp
@@ -1,105 +1,10 @@
 #include <iostream>
+#include "Circular_Queue.h"
 using namespace std;
 
-class CircularQueue
-{
-private:
-    int front, rear;
-    int queue[20];
-
-public:
-    CircularQueue() // Default constructor initialising front and rear to -1
-    {
-        front = -1;
-        rear = -1;
-    }
-    void enqueue(int);
-    void dequeue(int);
-    void display(int);
-    bool validation(int);
-};
-
-void CircularQueue ::enqueue(int n)
-{ 
-    // To check if queue is full 
-    // here front is ahead of rear
-    if ((front + 1) % n == ((rear + 1) % n) + 1)
-    {
-        cout << "Queue is full!\n";
-        return;
-    }
-    // If queue is empty we
-    // inititalise it to 0 
-    else if (front == -1)
-    {
-        front = 0;
-    }
-    // Incrementing rear by 1
-    rear = (rear + 1) % n;
-
-    cout << "Enter value :";
-    cin >> queue[rear];
-    cout << "Added :" << queue[rear] << endl;
-}
-
-void CircularQueue ::dequeue(int n)
-{
-    // Checking if queue is empty
-    if (front == -1)
-    {
-        cout << "Queue is empty!\n";
-        return;
-    }
-    int temp = front;
-
-    // If queue has only one element
-    // so f == r, we set f,r to -1 again
-    if (front == rear)
-    {
-        front = rear = -1;
-    }
-    //deleting from front
-    else
-    {
-        front = (front + 1) % n;
-    }
-    cout << "Deleted :" << queue[temp] << endl;
-}
-
-void CircularQueue ::display(int n)
-{
-    // If queue is empty
-    if (front == -1)
-    {
-        cout << "Queue is empty!\n";
-    }
-    else if (front > rear)
-    {
-        cout << "CircularQueue is as following \n";
-        // Since f>r we print from f to n first
-        for (int i = front; i < n; i++)
-        {
-            cout << queue[i] << " ";
-        }
-        // And then from 0 to r
-        for (int i = 0; i <= rear; i++)
-        {
-            cout << queue[i] << " ";
-        }
-    }
-    else
-    {
-        // Else print from f to r
-        for (int i = front; i <= rear; i++)
-        {
-            cout << queue[i] << " ";
-        }
-    }
-}
-
 int main()
-{
-    CircularQueue q;
+{ 
+    CircularQueue<int> q;
     int n;
     cout << "Enter size of circular queue: ";
     cin >> n;
