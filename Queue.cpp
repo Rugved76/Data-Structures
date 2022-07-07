0780b1e7b0c033f5814eaade7b846419493152e3
Rugved Wagh <89897353+Rugved76@users.noreply.github.com>
2022-07-07 18:12:08 +0530

    Update Queue.cpp

--- Queue.cpp
+++ Queue.cpp
@@ -5,20 +5,24 @@ using namespace std;
 int main()
 {
     int ch;
+    string r;
 
-    Queue <string> q;
+    Queue<string> q;
+
     while (1)
     {
-        cout << "1) Enqueue element to queue" << endl;
+        cout << "\n1) Enqueue element to queue" << endl;
         cout << "2) Dequeue element from queue" << endl;
         cout << "3) Display all the elements of queue" << endl;
-        cout << "4) Exit" << endl;
+        cout << "4) Reverse the queue" << endl;
+        cout << "5) Exit" << endl;
         cout << "Enter your choice : " << endl;
         cin >> ch;
         switch (ch)
         {
         case 1:
-            q.Enqueue();
+            cin >> r;
+            q.Enqueue(r);
             break;
         case 2:
             q.Dequeue();
@@ -27,6 +31,10 @@ int main()
             q.Display();
             break;
         case 4:
+            q.Reverse();
+            q.Display();
+            break;
+        case 5:
             cout << "Exit!" << endl;
             return 0;
         default:
